654b1db496c401810f39a1c9aeebc4fcb7280fe7
Andre Cowie <jpd3201@aut.ac.nz>
2017-05-31 16:29:02 +1200

    work on web scrapper on tuesday

--- webScrap.py
+++ webScrap.py
@@ -10,6 +10,7 @@ for x in soup.findAll("div", {"class":"listing__content"}):
 eve =[]
 for x in events:
     eve.append({'date':x.findAll("span")[0].get_text(),'location': x.findAll("span",{"itemprop":"location"})[0].findAll("span",{"itemprop":"name"})[0].get_text(),'title':x.findAll("a")[0].get_text(),'content':x.findAll("p")[0].get_text()})
-print(str(eve))
+for x in eve:
+	print (x['date'] + " " +x['location'] +" "+ x['title'])
 
 
