24a95dada1d71f4b77e7a0c0ad5928482e776989
Andre Cowie <andre.cowie@airnz.co.nz>
2017-08-07 19:58:28 +1200

    Commenting out my personal AWS configs

--- Graphing/application/models.py
+++ Graphing/application/models.py
@@ -5,9 +5,9 @@ import boto3
 import sys
 import os
 
-
-session = boto3.session.Session(profile_name="autrdproject")
-client = boto3.client('dynamodb')
+#Commenting out on master as they are my aws config :andre
+#session = boto3.session.Session(profile_name="autrdproject")
+#client = boto3.client('dynamodb')
 
 class KeysIndex(GlobalSecondaryIndex):
      class Meta:
@@ -41,10 +41,10 @@ class LocationTable(Model):
      countries = UnicodeSetAttribute(null=True)
      landsize = UnicodeAttribute(null=True)
 
-LocationTable._connection = LocationTable._get_connection()
-LocationTable._connection.connection._client = session.client('dynamodb', region_name='ap-southeast-2')
-LocationTypeTable._connection = LocationTypeTable._get_connection()
-LocationTypeTable._connection.connection._client = session.client('dynamodb', region_name='ap-southeast-2')
+#LocationTable._connection = LocationTable._get_connection()
+#LocationTable._connection.connection._client = session.client('dynamodb', region_name='ap-southeast-2')
+#LocationTypeTable._connection = LocationTypeTable._get_connection()
+#LocationTypeTable._connection.connection._client = session.client('dynamodb', region_name='ap-southeast-2')
 
 if not LocationTable.exists() and not LocationTypeTable.exists():
     LocationTable.create_table(read_capacity_units=50, write_capacity_units=50, wait=True)
