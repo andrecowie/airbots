477564ddf9845d2d5a534ce22a91eb5a552071ad
Andre Cowie <drecowie@gmail.com>
2017-08-17 00:02:29 +1200

    Demoready

--- Graphing/application/schema.py
+++ Graphing/application/schema.py
@@ -1,12 +1,18 @@
 import graphene
 from graphene import resolve_only_args
 import uuid
-from models import LocationTypeTable, LocationTable, CityTable, AirportTable, session
+from models import LocationTypeTable, LocationTable, CityTable, AirportTable
 
-continents = {}
-countries = {}
+continentstore = {}
+allContinents = False
+countrystore = {}
+allCountries = False
+citystore = {}
+allCitys = False
+airportstore = {}
+allAirports = False
 
-client = session.client('dynamodb')
+# client = session.client('dynamodb')
 
 class Location(graphene.Interface):
 	id = graphene.ID()
@@ -24,7 +30,20 @@ class Continent(graphene.ObjectType):
 	percentageoftotaleartharea = graphene.Field(lambda: graphene.Float)
 	countries = graphene.List(lambda: Country, name=graphene.String())
 	def resolve_countries(self, args, *_):
-		return [get_country(None, i) for i in self.countries]
+		if 'name' in args:
+			if 'countries' in self.__dict__:
+				countryid = get_countryorcontinent_id(args['name'])
+				if countryid is not None:
+					if countryid in self.countries:
+						return [get_country(countryid)]
+				return []
+		if 'countries' in self.__dict__:
+			if self.countries is not None:
+				if len(self.countries) > 1:
+					return get_batch_countries(self.countries)
+				elif len(self.countries) == 1:
+					return [get_countries(self.countries.pop())]
+		return []
 
 	def resolve_percentageoftotaleartharea(self, args, *_):
 		return float(self.landsize) / float(510000000)
@@ -42,6 +61,12 @@ class Country(graphene.ObjectType):
 	continent = graphene.Field(lambda: Continent)
 	cities =  graphene.List(lambda: City, name=graphene.String())
 	airports = graphene.List(lambda: Airport)
+	events = graphene.List(lambda: Event)
+	def resolve_event(self, args, *_):
+		if 'cities' in self.__dict__:
+			if self.cities is not None:
+				if len(self.cities) > 0:
+					return [Event(title="Example Event", location="Eden Park", coordinates=LatLng(latitude=174.7448,longitude=36.8750),type="Sport", description="Come to our example event",city=list(self.cities)[0], country=self.id)]
 	def resolve_airnzdestination(self, args, *_):
 		if 'airports' in self.__dict__:
 			return is_countryairnz_destination(self.airports)
@@ -52,14 +77,16 @@ class Country(graphene.ObjectType):
 		if 'name' in args:
 			if 'cities' in self.__dict__:
 				cityid = get_city_id(args['name'])
-				if cityid is not None:
-					if cityid in self.cities:
-						return [get_city(None,cityid)]
+				if self.cities is not None:
+					if cityid in list(self.cities):
+						return [get_city(cityid)]
 				return []
 		if 'cities' in self.__dict__:
 			if self.cities is not None:
 				if len(self.cities) > 1:
 					return get_batch_cities(self.cities)
+				elif len(self.cities) == 1:
+					return [get_city(self.cities.pop())]
 		return []
 	def resolve_airports(self, args, *_):
 		if 'airports' in self.__dict__:
@@ -78,6 +105,9 @@ class City(graphene.ObjectType):
 	airnzdestination = graphene.Field(lambda: graphene.Boolean)
 	airports = graphene.List(lambda: Airport)
 	country = graphene.Field(lambda: Country)
+	events = graphene.List(lambda: Event)
+	def resolve_events(self, args, *_):
+		return [Event(title="Example Event", location="Eden Park", coordinates=LatLng(latitude=174.7448,longitude=36.8750),type="Sport", description="Come to our example event",city=self.id, country=self.country)]
 	def resolve_airnzdestination(self, args, *_):
 		if 'airports' in self.__dict__:
 			return is_cityairnz_destination(self.airports)
@@ -94,7 +124,7 @@ class City(graphene.ObjectType):
 		return get_country(self.country)
 
 class Airport(graphene.ObjectType):
-	"""Airports adding more."""
+	"""Airports adding more. Will look to add airlines"""
 	class Meta:
 		interfaces = (Location,)
 	airnzdestination = graphene.Field(lambda: graphene.Boolean)
@@ -110,11 +140,30 @@ class Airport(graphene.ObjectType):
 	def resolve_cities(self, args, *_):
 		if 'cities' in self.__dict__:
 			if self.cities is not None:
-				if len(self.cities) > 1:
-					return get_batch_cities(self.cities)
-				elif len(self.cities) == 1:
-					return [get_city(self.cities[0])]
+
+				if len(list(self.cities)) > 1:
+					return get_batch_cities(list(self.cities))
+				elif len(list(self.cities)) == 1:
+					return [get_city(list(self.cities)[0])]
 		return []
+	def resolve_country(self, args, *_):
+		return get_country(self.country)
+
+class LatLng(graphene.ObjectType):
+	latitude = graphene.Float()
+	longitude = graphene.Float()
+
+class Event(graphene.ObjectType):
+	"""Our Event Type To Explore in the the coming sprint"""
+	title = graphene.String()
+	location = graphene.String()
+	coordinates = graphene.Field(LatLng)
+	type = graphene.String()
+	description = graphene.String()
+	city = graphene.Field(lambda: City)
+	country = graphene.Field(lambda: Country)
+	def resolve_city(self, args, *_):
+		return get_city(self.city)
 	def resolve_country(self, args, *_):
 		return get_country(self.country)
 
@@ -128,28 +177,48 @@ def get_airport_id(iata):
 	pass
 
 def get_batch_airports(ids):
-	airports = list(AirportTable.batch_get(ids))
 	airportreturn = []
+	global airportstore
+	idsStored = list(set(ids).intersection(set(airportstore.keys())))
+	for x in idsStored:
+		airportreturn.append(airportstore[x])
+		ids.remove(x)
+	airports = list(AirportTable.batch_get(ids))
 	if len(airports) > 0 :
 		for x in airports:
 			airportreturn.append(Airport(id=x.id, name=x.name, airnzdestination=x.airnzdestination,iata=x.iata, icao=x.icao, cities=x.cities, country=x.country))
 	return airportreturn
 
 def get_airport(id=None):
+	global allAirports
+	global airportstore
 	if id is None:
+		if allAirports:
+			return airportstore.values()
 		airports = []
 		y = AirportTable.scan()
 		for x in y:
-			airports.append(Airport(id=x.id, name=x.name, airnzdestination=x.airnzdestination,iata=x.iata, icao=x.icao, cities=x.cities, country=x.country))
+			airportstore[x.id] =  Airport(id=x.id, name=x.name, airnzdestination=x.airnzdestination,iata=x.iata, icao=x.icao, cities=x.cities, country=x.country)
+			airports.append(airportstore[x.id])
+		allAirports = True
 		return airports
 	else:
-		y = AirportTable.get(id)
-		return Airport(id=y.id, name=y.name, airnzdestination=y.airnzdestination,iata=y.iata, icao=y.icao, cities=y.cities, country=y.country)
+		if id in airportstore.keys():
+			return airportstore[id]
+		else:
+			y = AirportTable.get(id)
+			airportstore[y.id] = Airport(id=y.id, name=y.name, airnzdestination=y.airnzdestination,iata=y.iata, icao=y.icao, cities=y.cities, country=y.country)
+			return airportstore[y.id]
 
 
 def get_batch_cities(ids):
-	cities = list(CityTable.batch_get(ids))
+	global citystore
 	cityreturn = []
+	idsStored = list(set(ids).intersection(set(citystore.keys())))
+	for x in idsStored:
+		cityreturn.append(citystore[x])
+		ids.remove(x)
+	cities = list(CityTable.batch_get(ids))
 	if len(cities) > 0:
 		for x in cities:
 			cityreturn.append(City(id = x.id, name=x.name, airports=x.airports,country=x.country))
@@ -164,19 +233,34 @@ def get_city_id(name):
 	return None
 
 def get_city(id=None):
+	global allCitys
+	global citystore
 	if id is None:
+		if allCitys:
+			return citystore.values()
 		cities = []
 		y = CityTable.scan()
 		for z in y:
-			cities.append(City(id=z.id, name=z.name,airports=z.airports, country=z.country))
+			citystore[z.id] = City(id=z.id, name=z.name,airports=z.airports, country=z.country)
+			cities.append(citystore[z.id])
+		allCitys = True
 		return cities
 	elif id is not None:
-		y = CityTable.get(id)
-		return City(id=y.id, name=y.name, airports=y.airports,country=y.country)
+		if id in citystore.keys():
+			return citystore[id]
+		else:
+			y = CityTable.get(id)
+			citystore[y.id] = City(id=y.id, name=y.name, airports=y.airports,country=y.country)
+			return citystore[y.id]
 
 def get_batch_countries(ids):
-	countri = list(LocationTable.batch_get(ids))
+	global countrystore
 	countries = []
+	idsStored = list(set(ids).intersection(set(countrystore.keys())))
+	for x in idsStored:
+		countries.append(countrystore[x])
+		ids.remove(x)
+	countri = list(LocationTable.batch_get(ids))
 	if len(countri) > 0:
 		for x in countri:
 			countries.append(Country(id=x.id, name=x.name, airports=x.airports,population=x.population.replace(",", ""), continent=x.continent, cities=x.cities))
@@ -191,28 +275,43 @@ def get_countryorcontinent_id(name):
 	return None
 
 def get_country(id=None):
+	global allCountries
+	global countrystore
 	if id is None:
+		if allCountries:
+			return countrystore.values()
 		countries = []
 		clist = []
 		for x in LocationTypeTable.get(0).countries:
 			clist.append(x)
 		y = list(LocationTable.batch_get(clist))
+		allCountries=True
 		for z in y:
-			countries.append(Country(id=z.id, name=z.name, airports=z.airports,population=z.population.replace(",", ""), continent=z.continent, cities=z.cities))
+			countrystore[z.id] = Country(id=z.id, name=z.name, airports=z.airports,population=z.population.replace(",", ""), continent=z.continent, cities=z.cities)
+			countries.append(countrystore[z.id])
 		return countries
 	elif id is not None:
-		y = LocationTable.get(str(id))
-		if y.type == 'Country':
-			return Country(id=y.id, name=y.name, airports=y.airports, population=y.population.replace(",", ""), continent=y.continent, cities=y.cities)
+		if id in countrystore.keys():
+			return countrystore[id]
+		else:
+			y = LocationTable.get(str(id))
+			if y.type == 'Country':
+				countrystore[y.id] = Country(id=y.id, name=y.name, airports=y.airports, population=y.population.replace(",", ""), continent=y.continent, cities=y.cities)
+				return countrystore[y.id]
 	return None
 
 
 def get_continent(id=None):
+	global allContinents
+	global continentstore
 	if id is None:
+		if allContinents:
+			return continentstore.values()
 		continents = []
 		clist = []
 		for x in LocationTypeTable.get(0).continents:
 			clist.append(x)
+		allContinents = True
 		y = list(LocationTable.batch_get(clist))
 		for z in y:
 			countri = []
@@ -222,13 +321,17 @@ def get_continent(id=None):
 				if z.countries is not None:
 					for x in z.countries:
 						countri.append(x)
-			continents.append(Continent(id=z.id, name=z.name,
-										destination='True', landsize=z.landsize, population=z.population, countries=countri))
+			continentstore[z.id] = Continent(id=z.id, name=z.name, landsize=z.landsize, population=z.population, countries=countri)
+			continents.append(continentstore[z.id])
 		return continents
 	elif id is not None:
-		y=LocationTable.get(id)
-		if y.type == 'Continent':
-			return Continent(id=y.id, name=y.name,destination='True', landsize=y.landsize, population=y.population, countries=y.countries)
+		if id in continentstore.keys():
+			return continentstore[id]
+		else:
+			y=LocationTable.get(id)
+			if y.type == 'Continent':
+				continentstore[y.id] = Continent(id=y.id, name=y.name, landsize=y.landsize, population=y.population, countries=y.countries)
+				return continentstore[y.id]
 	return None
 
 class CityInput(graphene.InputObjectType):
@@ -247,18 +350,20 @@ class CreateCity(graphene.Mutation):
 		countryid = get_countryorcontinent_id(c_data.get('country'))
 		if countryid:
 			id = str(uuid.uuid4())
-			print (c_data.get('airports'))
-			if c_data.get('airports'):
-				print "MOney"
-				# CityTable(id=id, name=c_data.get('name'), country=countryid,population=c_data.get('population')).save()
-				# LocationTable.get(countryid).update({'cities':{'action':'add','value':id}}).save()
+			if not c_data.get('airports'):
+				print ("No Airports")
+				pop = str(c_data.get('population'))
+				CityTable(id=id, name=c_data.get('name'), country=countryid,population=pop).save()
+				print "City Saved"
+				LocationTable.get(countryid).update({'cities':{'action':'add','value':[id]}}).save()
+				print "Country Updated"
 			else:
 				validports = []
-				# for x in c_data.get('airports'):
-					# if get_airport(x):
-					# 	validports.append(x)
-				# CityTable(id=id, name=c_data.get('name'), country=countryid,population=c_data.get('population'),airports=validports).save()
-				# LocationTable.get(countryid).update({'cities':{'action':'add','value':id}}).save()
+				for x in c_data.get('airports'):
+					if get_airport(x):
+						validports.append(x)
+				CityTable(id=id, name=c_data.get('name'), country=countryid,population=c_data.get('population'),airports=validports).save()
+				LocationTable.get(countryid).update({'cities':{'action':'add','value':[id]}})
 			return CreateCity(get_city(id))
 		return CreateCity(City())
 
