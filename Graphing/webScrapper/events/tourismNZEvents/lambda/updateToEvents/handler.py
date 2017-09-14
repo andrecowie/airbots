3e433e7938a27fca4ae6d7a00b7c186099cd3d75
rachherlihy <rach.herlihy@gmail.com>
2017-09-14 15:51:53 +1200

    Finally?

--- Graphing/webScrapper/events/tourismNZEvents/lambda/updateToEvents/handler.py
+++ Graphing/webScrapper/events/tourismNZEvents/lambda/updateToEvents/handler.py
@@ -150,5 +150,5 @@ def main(event, context):
         CityTable(id=x['id'], name=x['name'], country=x['country']).save()
     for x in eventsToAdd:
         EventTable(id=x['id'], name=x['name'], url=x['url'], time=x['time'], date=x['date'], country=x['country'], city=x['city'], latitude=x['latitude'], longitude=x['longitude'],venuename=x['venuename'], category=x['category']).save()
-        LocationTable.get(x['country']).update({'events':{'action':"ADD", "value": x['id']}})
-        CityTable.get(x['city']).update({'events':{'action':"ADD", "value": x['id']}})
+        LocationTable.get(x['country']).update({'events':{'action':"ADD", "value": [x['id']]}})
+        CityTable.get(x['city']).update({'events':{'action':"ADD", "value": [x['id']]}})
