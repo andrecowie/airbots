63497ca4fd9d31c91a1396317f08c83689b6b48b
Andre Cowie <drecowie@gmail.com>
2017-09-18 10:43:59 +1200

    Bit of maintenance before merge

--- Graphing/application/app.py
+++ Graphing/application/app.py
@@ -8,11 +8,18 @@ app = Flask(__name__)
 app.add_url_rule(
     '/',
     view_func=GraphQLView.as_view(
-        'graphql',
+        'graphiql',
         schema=schema,
         graphiql=True
     )
 )
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema
+    )
+) 
 
 if __name__ == '__main__':
     app.run()
